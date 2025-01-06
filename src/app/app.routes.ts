8d51204314ad8df9cd1a2403b5f74a9ef5ae06be
NicolasIannello <nicolas.iannello99@gmail.com>
2025-01-06 14:16:54 -0300

    ruta users

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { InicioComponent } from './componentes/landing/inicio/inicio.component';
 import { authGuard } from './guard/auth.guard';
 import { PanelAdminComponent } from './componentes/panel-admin/panel-admin.component';
 import { LoginComponent } from './componentes/panel-admin/login/login.component';
+import { UsuariosComponent } from './componentes/panel-admin/usuarios/usuarios.component';
 
 export const routes: Routes = [
     { path: '', component: LandingComponent, children: [
@@ -11,7 +12,7 @@ export const routes: Routes = [
     ] },
     { path: 'panelAdmin', component: PanelAdminComponent , children: [
         { path: '', component: LoginComponent },
-        //{ path: 'autos', component: UsuariosComponent, canActivate:[authGuard] },
+        { path: 'usuarios', component: UsuariosComponent, canActivate:[authGuard] },
         { path: '**',   redirectTo: '', pathMatch: 'full' },
     ]},
     { path: '**',   redirectTo: '', pathMatch: 'full' },
