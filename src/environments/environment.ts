bd3f46c843ea90b75c48edf73ab6c26394806984
NicolasIannello <nicolas.iannello99@gmail.com>
2025-01-16 13:31:05 -0300

    dw

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  //base_url:'https://vps-4154293-x.dattaweb.com/sta',
-  base_url:'http://localhost:3000/sta',
+  base_url:'https://vps-4154293-x.dattaweb.com/sta',
+  //base_url:'http://localhost:3000/sta',
   socketToken: 'z8ddwa7pws9t',
   recaptcha: {
     siteKey: '6Let_k4qAAAAAIHQe6HtHzYwosJ58y4feuaKRmRu',
