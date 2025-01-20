02d95d3c7c64a5cb40165ae8caeaae3391dad5aa
NicolasIannello <nicolas.iannello99@gmail.com>
2025-01-20 17:06:24 -0300

    licitaciones

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: true,
   base_url:'https://vps-4154293-x.dattaweb.com/sta',
   //base_url:'http://localhost:3000/sta',
-  base_url2:'https://vps-4154293-x.dattaweb.com',
-  //base_url2:'http://localhost:3000',
+  licitaciones_url:'https://vps-4154293-x.dattaweb.com/api',
+  //licitaciones_url:'http://localhost:3000/api',
   socketToken: 'z8ddwa7pws9t',
   recaptcha: {
     siteKey: '6Let_k4qAAAAAIHQe6HtHzYwosJ58y4feuaKRmRu123',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,8 @@ export const environment = {
   production: false,
   base_url:'https://vps-4154293-x.dattaweb.com/sta',
   //base_url:'http://localhost:3000/sta',
+  licitaciones_url:'https://vps-4154293-x.dattaweb.com/api',
+  //licitaciones_url:'http://localhost:3000/api',
   socketToken: 'z8ddwa7pws9t',
   recaptcha: {
     siteKey: '6Let_k4qAAAAAIHQe6HtHzYwosJ58y4feuaKRmRu',
