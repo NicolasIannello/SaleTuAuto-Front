20335ad3c30863b446451f65b366d15bc14f7db1
NicolasIannello <nicolas.iannello99@gmail.com>
2025-02-08 13:12:13 -0300

    dist

--- dist/sta/server/main.server.mjs
+++ dist/sta/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-VIQGSTHA.mjs";import"./chunk-V5QNZL2J.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-AW6PDYBT.mjs";import"./chunk-V5QNZL2J.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
