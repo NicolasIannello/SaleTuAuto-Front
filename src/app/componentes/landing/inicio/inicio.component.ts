4e2afb8c747c813be9f1c2a0dccfdf35d15250bc
NicolasIannello <nicolas.iannello99@gmail.com>
2025-02-17 12:54:04 -0300

    contacto comp

--- src/app/componentes/landing/inicio/inicio.component.ts
+++ src/app/componentes/landing/inicio/inicio.component.ts
@@ -7,20 +7,15 @@ import { ListaAutosComponent } from "../../autos/lista-autos.component";
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [CommonModule, RouterModule, ListaAutosComponent],
+  imports: [CommonModule, RouterModule, ListaAutosComponent, ContactoComponent],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
 export class InicioComponent {
   @Input() widthC: number | undefined;
   @Input() cap: number | undefined;
-  number:string="+5491127605336";
 
   constructor(private scroller: ViewportScroller) {}
-  
-  wspW(){
-    window.open('https://wa.me/'+this.number);
-  }
 
   vehiculos(){
     this.scroller.scrollToAnchor('vehiculos');
